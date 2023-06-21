705fe19d1a04c391f3c78c9aa59189cd1c773eed
Prabish Prakash <prabish19@gmail.com>
2023-06-21 01:20:25 -0400

    Modified to add changed to test GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 
+declare var gtag: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +14,9 @@ export class AppComponent {
   constructor(router: Router){
     const navEndEvents = router.events.pipe(filter(event => event instanceof NavigationEnd))
     navEndEvents.subscribe((event: NavigationEnd) => {
-
+      gtag('config', 'G-ZLWVNCME1T', {
+        'page_path': event.urlAfterRedirects
+      });
     });
   }
 }
