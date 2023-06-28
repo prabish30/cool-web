4b0774a3a449dcaf419d09acdd6f450584c914ea
Prabish Prakash <prabish30@gmail.com>
2023-06-28 10:45:50 -0400

    Update to add console info statements in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ declare var gtag: any;
 export class AppComponent {
   title = 'cool-web';
   constructor(router: Router){
-    console.log('INSIDE APP CONSTRUCTOR');
+    console.info('INSIDE APP CONSTRUCTOR');
     const navEndEvents = router.events.pipe(filter(event => event instanceof NavigationEnd))
     navEndEvents.subscribe((event: NavigationEnd) => {
-      console.log('URL AFTER REDIRECTS:', event.urlAfterRedirects);
+      console.info('URL AFTER REDIRECTS:', event.urlAfterRedirects);
       gtag('config', 'G-Y090ETQNZ4', {
         'page_path': event.urlAfterRedirects
       });
