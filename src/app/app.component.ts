4aab53a23151a8dcc6842a6ebb6f47b20670b7ea
Prabish Prakash <prabish30@gmail.com>
2023-06-28 10:45:07 -0400

    Adding console log in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ declare var gtag: any;
 export class AppComponent {
   title = 'cool-web';
   constructor(router: Router){
+    console.log('INSIDE APP CONSTRUCTOR');
     const navEndEvents = router.events.pipe(filter(event => event instanceof NavigationEnd))
     navEndEvents.subscribe((event: NavigationEnd) => {
       console.log('URL AFTER REDIRECTS:', event.urlAfterRedirects);
